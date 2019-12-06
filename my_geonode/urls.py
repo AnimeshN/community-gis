2eddc3499852c4618974024c6b541da6b7bd424b
AnimeshN <animesh.nauti@gmail.com>
2019-12-06 19:43:13 +0530

    small changes

--- my_geonode/urls.py
+++ my_geonode/urls.py
@@ -25,7 +25,7 @@ from geonode.urls import urlpatterns
 
 urlpatterns += [
 ## include your urls here
-    url(r'^dashboard/', include('dashboard.urls')),
+    url(r'^dashboard/', include('dashboard.urls')),    
 ]
 
 urlpatterns = [
