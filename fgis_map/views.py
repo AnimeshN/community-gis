2eca1bac4d1f21748832a3242671f41282ac6321
AnimeshN <animesh.nauti@gmail.com>
2020-08-03 10:59:37 +0530

    added siteurl in context

--- fgis_map/views.py
+++ fgis_map/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def fgis_map(request):
-    return render(request,'fgis_map/map.html')
+    context = {'site_url':settings.MY_SITE_URL}
+    return render(request,'fgis_map/map.html',context)
